24e44e0016b0523d510820034e0f03cf744f4c11
Bianca <s59416@beuth-hochschule.de>
2015-11-01 15:58:10 +0100

    excersices 2 task 1, SensorActivity added SensorEventListener

--- app/src/main/java/de/bbg/ema_excercises/SensorActivity.java
+++ app/src/main/java/de/bbg/ema_excercises/SensorActivity.java
@@ -1,13 +1,23 @@
 package de.bbg.ema_excercises;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class SensorActivity extends AppCompatActivity {
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,28 @@ public class SensorActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        SensorEventListener listener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                System.out.println("onSensorChanged");
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                System.out.println("onAccuracyChanged");
+            }
+        };
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(listener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        System.out.println("mSensorManager " + mSensorManager.toString());
+        System.out.println("mSensor " + mSensor.toString());
+        System.out.println("listener " + listener.toString());
+
     }
 
 }
