7d861f5c191a4dcec53f61c844ad5e47f03a72e2
Bianca <s59416@beuth-hochschule.de>
2015-11-01 15:40:04 +0100

    excersice 2 task 1 added SensorActivity

--- app/src/main/java/de/bbg/ema_excercises/MainActivity.java
+++ app/src/main/java/de/bbg/ema_excercises/MainActivity.java
@@ -1,13 +1,16 @@
 package de.bbg.ema_excercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +21,40 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });*/
+
+        Button sensorButton = (Button)findViewById(R.id.btSensor);
+        sensorButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                openSensor();
+                return true;
+            }
         });
+
+
+        /*View mainLayout = findViewById(R.id.content_main); // root View id from that link
+        View myView = mainLayout.findViewById( R.id. );*/
+
+
+
     }
+
+    public void openSensor(){
+        Intent intent = new Intent(this, SensorActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
